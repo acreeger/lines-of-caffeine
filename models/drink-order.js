0f9bd191bb4563f302612edf1ce9b92f4f32d312
Adam Creeger <adamcreeger@gmail.com>
2013-06-09 22:54:39 -0700

    Adding API support and testsgit st for querying by contact #30.

--- models/drink-order.js
+++ models/drink-order.js
@@ -6,90 +6,93 @@ var mongoose = require('mongoose')
   , util = require('../common/util')
   , CustomValidationError = require('../common/errors').CustomValidationError;
 
-var DrinkOrderSchema = new Schema({
-  customer : {
-    firstName: {type:String, required:true},
-    lastName: {type:String, required:true},
-    cellPhone: {type: String, match: constants.US_PHONE_VALIDATION_REGEX},
-    emailAddress: {type: String, match: constants.EMAIL_VALIDATION_REGEX}
-  },
-  drinks : [{
-    strength : {type:String, required:true},
-    drinkType : {type:String, required:true},
-    milk : {type:String, required:true},
-    specialInstructions : String
-  }],
-  status : {type: String, default: orderConstants.STATUS_NEW},
-  station : {type: String, default: "1"},
-  assignee : {type: Number, default: -1},
-  date: { type: Date, default: Date.now },
-  dateStarted: Date,
-  dateCompleted: Date,
-  estimatedCompletionWait: Number
-});
 
-var getContactInfo = function() {
-  if (this.hasValidEmailAddress()) {
-    return this.emailAddress;
-  } else {
-    return this.cellPhone;
+var init = function(connection) {
+  var DrinkOrderSchema = new Schema({
+    customer : {
+      firstName: {type:String, required:true},
+      lastName: {type:String, required:true},
+      cellPhone: {type: String, match: constants.US_PHONE_VALIDATION_REGEX, index : true},
+      emailAddress: {type: String, match: constants.EMAIL_VALIDATION_REGEX, index : true}
+    },
+    drinks : [{
+      strength : {type:String, required:true},
+      drinkType : {type:String, required:true},
+      milk : {type:String, required:true},
+      specialInstructions : String
+    }],
+    status : {type: String, default: orderConstants.STATUS_NEW},
+    station : {type: String, default: "1"},
+    assignee : {type: Number, default: -1},
+    date: { type: Date, default: Date.now },
+    dateStarted: Date,
+    dateCompleted: Date,
+    estimatedCompletionWait: Number
+  });
+
+  var getContactInfo = function() {
+    if (this.hasValidEmailAddress()) {
+      return this.emailAddress;
+    } else {
+      return this.cellPhone;
+    }
   }
-}
 
-var setContactInfo = function(value) {
-  if (util.validateEmailAddress(value)) {
-    this.customer.emailAddress = value;
-  } else if (util.validateUSPhoneNumber(value)) {
-    this.customer.cellPhone = value;
+  var setContactInfo = function(value) {
+    if (util.validateEmailAddress(value)) {
+      this.customer.emailAddress = value;
+    } else if (util.validateUSPhoneNumber(value)) {
+      this.customer.cellPhone = value;
+    }
   }
-}
 
-DrinkOrderSchema.virtual('cellPhone').get(function () {
-  return this.customer.cellPhone;
-});
+  DrinkOrderSchema.virtual('cellPhone').get(function () {
+    return this.customer.cellPhone;
+  });
 
-DrinkOrderSchema.virtual('emailAddress').get(function () {
-  return this.customer.emailAddress;
-});
+  DrinkOrderSchema.virtual('emailAddress').get(function () {
+    return this.customer.emailAddress;
+  });
 
-DrinkOrderSchema.virtual('fullName').get(function() {
-  return this.customer.firstName + ' ' + this.customer.lastName;
-})
+  DrinkOrderSchema.virtual('fullName').get(function() {
+    return this.customer.firstName + ' ' + this.customer.lastName;
+  })
 
-DrinkOrderSchema.virtual('contactInfo').get(getContactInfo).set(setContactInfo);
+  DrinkOrderSchema.virtual('contactInfo').get(getContactInfo).set(setContactInfo);
 
-DrinkOrderSchema.pre("validate", function(next) {
-  var err;
-  if (!this.customer.emailAddress && !this.customer.cellPhone) {
-    err = new CustomValidationError("Every order must have an email address OR a cell phone number");
+  DrinkOrderSchema.pre("validate", function(next) {
+    var err;
+    if (!this.customer.emailAddress && !this.customer.cellPhone) {
+      err = new CustomValidationError("Every order must have an email address OR a cell phone number");
+    }
+    next(err);
+  })
+
+  DrinkOrderSchema.pre('save', function (next) {
+    if (this.hasValidCellPhone()) {
+      this.customer.cellPhone = util.normalizeUSPhoneNumber(this.customer.cellPhone);
+    }
+    next();
+  });
+
+  DrinkOrderSchema.methods.hasValidEmailAddress = function () {
+    return this.emailAddress && util.validateEmailAddress(this.emailAddress);
   }
-  next(err);
-})
 
-DrinkOrderSchema.methods.hasValidEmailAddress = function () {
-  return this.emailAddress && util.validateEmailAddress(this.emailAddress);
-}
+  DrinkOrderSchema.methods.hasValidCellPhone = function (){
+    return this.cellPhone && util.validateUSPhoneNumber(this.cellPhone);
+  }
 
-DrinkOrderSchema.methods.hasValidCellPhone = function (){
-  return this.cellPhone && util.validateUSPhoneNumber(this.cellPhone);
-}
+  DrinkOrderSchema.statics.getOrdersForUser = function(contactInfo, limit, cb) {
+    //TODO: Need to normalize old phone-numbers (including existing records... Eek!)
+    //TODO: normalize phone number (add in util)
+    contactInfo = util.normalizeUSPhoneNumber(contactInfo);
+    this.find({$or : [{"customer.cellPhone" : contactInfo}, {"customer.emailAddress" : contactInfo}]}, null, {order: "-date", limit:limit}, cb)
+    // cb(null, [])
+  }
 
-mongoose.model('DrinkOrder', DrinkOrderSchema);
+  connection = connection || mongoose;
+  connection.model('DrinkOrder', DrinkOrderSchema);
+};
 
-// var ArticleSchema = new Schema({
-//   title: {type : String, default : '', trim : true},
-//   body: {type : String, default : '', trim : true},
-//   user: {type : Schema.ObjectId, ref : 'User'},
-//   comments: [{
-//     body: { type : String, default : '' },
-//     user: { type : Schema.ObjectId, ref : 'User' },
-//     createdAt: { type : Date, default : Date.now }
-//   }],
-//   tags: {type: [], get: getTags, set: setTags},
-//   image: {
-//     cdnUri: String,
-//     files: []
-//   },
-//   createdAt  : {type : Date, default : Date.now}
-// })
-
+module.exports = init;
---
+++ test/routes/test-drink-orders.js
@@ -0,0 +1,284 @@
+var app = require('../../app');
+var request = require('supertest');
+
+var helper = require("../drink-order-helper")
+
+describe("drink-order routes" ,function() {
+  describe('GET /api/order/searchByContact', function(){
+    beforeEach(function(done){
+      helper.removeAllOrders(function(err) {
+        if (err) return done(err);
+        helper.createTestOrders(["testemail@test.com","someothertest@email.com","415.767.8448","646-766-3444",
+                          "testemail@test.com","someothertest@email.com","(415) 7678448","+1 (646) 766-3444",
+                          "testemail@test.com","someothertest@email.com","(415) 767-8448","6467663444", "testemail@test.com"], done);
+      });
+    })
+    it("should return empty list for non-existant email", function(done) {
+      var contactInfo = "idontexist@here.com";
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(contactInfo);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(0);
+            done();
+          }
+        });
+    });
+    it("should return empty list for non-existant cellPhone", function(done) {
+      var contactInfo = "555 999 9999";
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(contactInfo);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(0);
+            done();
+          }
+        });
+    });
+    it("should return the correct 1 order for a valid email when no limit is specified in descending chrono order", function(done) {
+      var email = "someothertest@email.com";
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(email);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(1);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("emailAddress", email)
+            done();
+          }
+        });
+    });
+    it("should return the correct number of orders for a valid email (3) in descending chrono order", function(done) {
+      var email = "someothertest@email.com";
+      var url = "/api/order/searchByContact?limit=3&contact=" + encodeURIComponent(email);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(3);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("emailAddress", email)
+            done();
+          }
+        });
+    });
+    it("should return the correct number of orders for a valid email (3, but requested 5) in descending chrono order", function(done) {
+      var email = "someothertest@email.com";
+      var url = "/api/order/searchByContact?limit=5&contact=" + encodeURIComponent(email);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(3);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("emailAddress", email)
+            done();
+          }
+        });
+    });
+    it("should return the all orders for a valid email (4) when limit is 0", function(done) {
+      var email = "testemail@test.com";
+      var url = "/api/order/searchByContact?limit=0&contact=" + encodeURIComponent(email);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(4);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("emailAddress", email)
+            done();
+          }
+        });
+    });
+    it("should return the all orders for a valid email (4) when limit is < 0", function(done) {
+      var email = "testemail@test.com";
+      var url = "/api/order/searchByContact?limit=-1&contact=" + encodeURIComponent(email);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(4);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("emailAddress", email)
+            done();
+          }
+        });
+    });
+    it("should return the only 1 order when limit is not a valid number", function(done) {
+      var email = "testemail@test.com";
+      var url = "/api/order/searchByContact?limit=fgsdfgdf&contact=" + encodeURIComponent(email);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(1);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("emailAddress", email)
+            done();
+          }
+        });
+    });
+    it("should return the correct number of orders for a valid cellPhone", function(done) {
+      var cellPhone = "4157678448"
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(cellPhone);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(1);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("cellPhone", "+14157678448")
+            done();
+          }
+        });
+    });
+    it("should return the correct order for a cellPhone number, even when it is in a different format", function(done) {
+      var cellPhone = "(415) 767 8448"
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(cellPhone);
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(1);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("cellPhone", "+14157678448")
+            done();
+          }
+        });
+    });
+    it("should return the correct number of orders for a valid cellPhone (3) in descending chrono order", function(done) {
+      var cellPhone = "4157678448"
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(cellPhone) + "&limit=3";
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(3);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("cellPhone", "+14157678448")
+            done();
+          }
+        });
+    });
+    it("should return the correct number of orders for a valid cellPhone (3, but requested 5) in descending chrono order", function(done) {
+      var cellPhone = "4157678448"
+      var url = "/api/order/searchByContact?contact=" + encodeURIComponent(cellPhone) + "&limit=5";
+      request(app)
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err)
+          } else {
+            var obj = res.body;
+            obj.should.be.a("object");
+            obj.should.have.property("success", true)
+            obj.should.have.property("data")
+            obj.data.should.have.length(3);
+            obj.data[0].should.have.property("customer");
+            obj.data[0].customer.should.have.property("cellPhone", "+14157678448")
+            done();
+          }
+        });
+    });
+    it("should return 400 when no contact is provided", function(done) {
+      var url = "/api/order/searchByContact";
+      request(app)
+        .get(url)
+        .expect(400, done);
+    });
+  });
+});
+  
