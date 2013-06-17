ce585ed4b43cac953ac7cdcf17273026711dd4ef
Adam Creeger <adamcreeger@gmail.com>
2013-06-17 00:45:35 -0700

    Formatting phone numbers in the report consistently

--- public/js/shared.js
+++ public/js/shared.js
@@ -1,3 +1,14 @@
+(function ($) {
+  var regex = /\+1(\d{3})(\d{3})(\d{4})/
+  $.fn.renderUSPhoneNumber = function() {
+    return this.each(function() {
+      var $this = $(this);
+      var newVal = $this.text().replace(regex, "($1) $2-$3")
+      $this.text(newVal);
+    }); 
+  }
+}(jQuery));
+
 var COFFEE = COFFEE || {};
 
 Array.prototype.diff = function(a) {
